5ed75e113de0fd002d241e2b7acd9f83801107f8
artemian4ik <36512934+artemian4ik@users.noreply.github.com>
2020-05-20 19:32:17 +0300

    Add files via upload

---
+++ laba5/laba1/laba51.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+
+int main()
+{
+	HANDLE mutex = CreateMutexA(NULL, FALSE, "MyMutex");
+
+	if (WaitForSingleObject(mutex, 0) == WAIT_OBJECT_0)
+	{
+		printf("Program started! Press any key to stop!");
+		getchar();
+		ReleaseMutex(mutex);
+	}
+	else
+	{
+		printf("Can't start program!");
+		getchar();
+	}
+
+	CloseHandle(mutex);
+	return 0;
+}
